1e4d584496dd04763e5aaf7267254b405c04777a
Paul Clayton  Smith <paulclaytonsmith@gmail.com>
2025-03-16 09:59:45 -0700

    Grid flexing

--- src/components/Chart/Grid.tsx
+++ src/components/Chart/Grid.tsx
@@ -1,42 +1,7 @@
 'use client';
 
 import React from 'react';
-import styled from 'styled-components';
-
-const GridContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-end;
-  width: 100%;
-  border: 2px solid #FFFFFF;
-`;
-
-const Tile = styled.div`
-  width: 96px;
-  height: 96px;
-  border: 1px solid #FFFFFF;
-  position: relative;
-  
-  &::before, &::after {
-    content: '';
-    position: absolute;
-    background: rgba(255, 255, 255, 0.5);
-  }
-  
-  &::before {
-    width: 2px;
-    height: 100%;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-  
-  &::after {
-    width: 100%;
-    height: 2px;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-`;
+import './Grid.css';
 
 interface GridProps {
   columns: number;
@@ -45,10 +10,13 @@ interface GridProps {
 
 export const Grid: React.FC<GridProps> = ({ columns, rows }) => {
   return (
-    <GridContainer>
+    <div 
+      className="grid-container"
+      style={{ '--columns': columns } as React.CSSProperties}
+    >
       {Array.from({ length: columns * rows }).map((_, index) => (
-        <Tile key={index} />
+        <div key={index} className="grid-tile" />
       ))}
-    </GridContainer>
+    </div>
   );
 }; 
